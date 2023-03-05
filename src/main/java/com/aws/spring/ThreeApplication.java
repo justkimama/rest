00c90ed8a3f8a4a25df1024b91f3636dc78ca500
justkimama <rlatjsgh1000@naver.com>
2023-03-06 01:32:23 +0900

    Changes

--- src/main/java/com/aws/spring/ThreeApplication.java
+++ src/main/java/com/aws/spring/ThreeApplication.java
@@ -3,7 +3,7 @@ package com.aws.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.aws.spring.controller"})
 public class ThreeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThreeApplication.class, args);
