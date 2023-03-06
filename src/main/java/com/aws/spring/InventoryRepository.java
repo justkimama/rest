0962d7b9c03cba6d9b66917d6372dd108ee7f1e3
justkimama <rlatjsgh1000@naver.com>
2023-03-06 10:36:56 +0900

    Changes

--- src/main/java/com/aws/spring/InventoryRepository.java
+++ src/main/java/com/aws/spring/InventoryRepository.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     @Query(value = "SELECT * FROM aws_table a WHERE a.name = binary :name"    //For NativeQuery
             , nativeQuery = true)
-    String findName(String name);
+    String findName(@Param("name") String name);
 
     @Query(value = "SELECT * FROM aws_table"
             , nativeQuery = true)
@@ -25,19 +26,19 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     @Query(value = "SELECT * FROM aws_table WHERE name = :name"
             , nativeQuery = true)
-    List<Inventory> findAllAmountByName(String name);
+    List<Inventory> findAllAmountByName(@Param("name") String name);
 
     @Modifying
     @Transactional
     @Query(value = "UPDATE aws_table a SET a.amount = a.amount + :amount WHERE a.name = :name"
             , nativeQuery = true)
-    int updateAmount(int amount, String name);
+    int updateAmount(@Param("amount") int amount, @Param("name") String name);
 
     @Modifying
     @Transactional
     @Query(value = "UPDATE aws_table a SET a.amount = a.amount - :amount, a.price = a.price + :price WHERE a.name = :name"
             , nativeQuery = true)
-    int updateAmountAndPrice(int amount, int price, String name);
+    int updateAmountAndPrice(@Param("amount") int amount, @Param("price") int price, @Param("name") String name);
 
     @Modifying
     @Transactional
