6b5406ae93889781b4313d2852c9ebb19e88b887
justkimama <rlatjsgh1000@naver.com>
2023-03-06 15:32:37 +0900

    Changes

--- src/main/java/com/aws/spring/BasicController.java
+++ src/main/java/com/aws/spring/BasicController.java
@@ -1,16 +1,17 @@
 package com.aws.spring;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BasicController {
     @GetMapping("/")
-    public String home (){
-        return "AWS";
+    public ResponseEntity home (){
+        return ResponseEntity.ok("AWS");
     }
     @GetMapping("/secret")
-    public String secret (){
-        return "SUCCESS";
+    public ResponseEntity secret (){
+        return ResponseEntity.ok("SUCCESS");
     }
 }
